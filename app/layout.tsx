9f92bcaf9a569216503a86c0389cf8069708bf94
Aditya Phasu <adityaphasu919@gmail.com>
2024-01-01 14:45:31 +0530

    Update metadata and add Posts component to page

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Blog",
-  description: "A blog built with Next.js",
+  description: "Created by Aditya Phasu",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
