b6ad3af23b7907aebc75d5cb632240ae97692f8a
Aditya Phasu <adityaphasu919@gmail.com>
2024-01-06 20:55:01 +0530

    Fix typo

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
         Hello and Welcome 😀 &nbsp;
         <span className="whitespace-nowrap">
-          I'm <span className="font-bold">Adi</span>
+          I&apso;m <span className="font-bold">Adi</span>
         </span>
       </p>
       <Posts />
